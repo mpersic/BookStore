97cbdb38935c41d617a73ae07f0b45d9bcf166bf
Matej <matej.persic@live.com>
2020-12-27 22:13:02 +0100

    initial UI commit

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -6,16 +6,46 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dobrodošli u online knjižaru!\n\nJeste li korisnik? (Y/N)");
-            bool isUser = Console.ReadLine() == "Y";
-            Console.WriteLine(isUser);
+            Console.WriteLine("Dobrodošli u online knjižaru!\n\nJeste li korisnik? (D/N)");
+            string isUser = Console.ReadLine();
 
-            while (true)
+            if (isUser == "D")
             {
-                // sta god korisnik radi, jedna od opcija -> exit
+                Console.WriteLine("IdKupca: ");
+                //učitavanje
+                Console.WriteLine("\nAdresa: ");
+                //
+                Console.WriteLine("\nBroj mobitela: ");
+                //
+                Console.WriteLine("\nEmail: ");
+                //
+                Console.WriteLine("\nIme i prezime: ");
+                //
+                string izbornik = "Izbornik:\n     -Prikaži košaricu (K)\n     -Dodaj u košaricu (D)\n     -Ukloni iz košarice (U)\n     -Prikaži sve knjige (P)\n     -Filter (F)\n     -Izađi iz programa (X)\n";
+                string filter = "Filter:\n     -Ime knjige (K)\n     -Ime autora (A)\n     -Ime izdavača (I)\n     -Godina izadnja (G)\n     -Izađi iz programa (X)\n";
 
-                
+                Console.WriteLine(izbornik);
+                string response = Console.ReadLine();
+                while (true)
+                {
+                    if (response == "K") { }
+                    if (response == "D") { }
+                    if (response == "U") { }
+                    if (response == "P") { }
+                    if (response == "F")
+                    {
+                        Console.WriteLine(filter);
+                        response = Console.ReadLine();
+                        if (response == "K") ;
+                        if (response == "A") ;
+                        if (response == "I") ;
+                        if (response == "G") ;
+                        if (response == "X") ;
+                    }
+                    if (response == "X") { }
+                }
             }
+            if (isUser == "N") { }
         }
     }
 }
